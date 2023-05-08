f1b2c95653f152f37475bec25f2f29fe1231b702
herudi <herudi7@gmail.com>
2023-05-08 15:29:01 +0700

    deploy

--- deploy.ts
+++ deploy.ts
@@ -1,5 +1,5 @@
-import nhttp from "https://deno.land/x/nhttp@1.2.13/mod.ts";
-import serveStatic from "https://deno.land/x/nhttp@1.2.13/lib/serve-static.ts";
+import nhttp from "https://deno.land/x/nhttp@1.2.15/mod.ts";
+import serveStatic from "https://deno.land/x/nhttp@1.2.15/lib/serve-static.ts";
 
 const app = nhttp();
 
@@ -7,12 +7,8 @@ app.use(serveStatic("build", {
   setHeaders({ response }) {
     response.setHeader("x-powered-by", "nhttp");
   },
+  spa: true
 }));
-
-app.on404(async ({ response }) => {
-  response.type("html");
-  return await Deno.readFile("build/404.html");
-});
 
 app.listen(8080, (err, info) => {
   if (err) throw err;
