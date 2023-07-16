f510d15cc0c55a316dcec60313667e47d3ae47c9
herudi <herudi7@gmail.com>
2023-07-16 11:27:45 +0700

    404rem

--- deploy.ts
+++ deploy.ts
@@ -3,15 +3,6 @@ import serveStatic from "https://deno.land/x/nhttp@1.3.0/lib/serve-static.ts";
 
 const app = nhttp();
 
-app.get("/404", (rev) => {
-  rev.response.type("html");
-  const stat = Deno.statSync("build/404.html");
-  if (stat.isFile) {
-    console.log("file");
-  }
-  return Deno.readFileSync("build/404.html");
-})
-
 app.use(serveStatic("build", {
   setHeaders({ response }, path) {
     let cc = "public, max-age=604800, immutable";
