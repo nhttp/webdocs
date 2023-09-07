466fa7cfc1dc24b7d732a4281afc576d3c533b50
herudi <herudi7@gmail.com>
2023-09-07 14:08:38 +0700

    deploy.ts

--- deploy.ts
+++ deploy.ts
@@ -3,34 +3,18 @@ import serveStatic from "https://deno.land/x/nhttp@1.3.9/lib/serve-static.ts";
 
 const app = nhttp();
 
-app.use(
-  (rev, next) => {
-    const accept = rev.headers.get("accept");
-    const userAgent = rev.headers.get("user-agent");
-    if (
-      !accept?.includes("text/html") &&
-      userAgent?.toLowerCase().startsWith("deno")
-    ) {
-      return new Response("create_nhttp", {
-        headers: { "Location": "https://esm.sh/create-nhttp" },
-        status: 307,
-      });
+app.use(serveStatic("build", {
+  setHeaders({ response }, path) {
+    if (!path.endsWith(".html")) {
+      response.setHeader(
+        "cache-control",
+        "public, max-age=604800, immutable",
+      );
     }
-    return next();
+    response.setHeader("x-powered-by", "nhttp");
   },
-  serveStatic("build", {
-    setHeaders({ response }, path) {
-      if (!path.endsWith(".html")) {
-        response.setHeader(
-          "cache-control",
-          "public, max-age=604800, immutable",
-        );
-      }
-      response.setHeader("x-powered-by", "nhttp");
-    },
-    spa: true,
-  }),
-);
+  spa: true,
+}));
 
 app.listen(8080, (err, info) => {
   if (err) throw err;
